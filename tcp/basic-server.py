1293a808fd70584caa6a61aba4f2015232346dea
ari <ariannahblack@gmail.com>
2024-10-17 15:57:47 -0400

    clean up code

--- tcp/basic-server.py
+++ tcp/basic-server.py
@@ -29,14 +29,13 @@ import pandas as pd         ## to read and load the txt file
 ## server constants
 SERVERHOST = ''     ## server machine, '' means local host
 SERVERPORT = 12000  ## a non-reserved port number to listen on
-# DF = pd.read_csv('1442907.txt', sep=' ', header=None)    ## dataframe of word list
-# serie = df.transpose()[0]
-DF = pd.read_csv('1442907.txt', sep=' ', header=None, names=['word'])    ## dataframe of word list
-DF = DF['word']
 
+DF = pd.read_csv('1442907.txt', sep=' ', header=None, names=['word'])    ## dataframe of word list
+DF = DF['word'] ## to convert from a dataframe to a series object
 
 def startServer():
-    ''' starts server by creating socket and calling handler methods '''
+    ''' start server by creating socket and calling handler methods '''
+
     ## create socket
     serverSocket = socket(AF_INET,SOCK_STREAM)  ## TCP socket object
     serverSocket.bind((SERVERHOST,SERVERPORT))  ## bind server socket to port
@@ -48,60 +47,61 @@ def startServer():
 
 def handleClient(connection, serverSocket):
     ''' receive and perform wildcard query from client '''
-    request = connection.recv(1024)         ## accept query from the connection 
-    num_matches, matches, query = performQuery(request)     ## perform and unpack the received query
-    sendResponse(serverSocket, connection, num_matches, matches, query)   ## send response through the connection
-    connection.close()                      ## close connection after just one query
+
+    ## accept query from the connection 
+    request = connection.recv(1024)
+
+    ## perform and unpack the received query
+    num_matches, matches, query = performQuery(request)
+
+    ## send response through the connection
+    sendResponse(connection, num_matches, matches, query)
+
+    ## close connection after just one query
+    connection.close()                      
 
 def performQuery(request):
     ''' unpack and perform wildcard query logic '''
 
     ## decode query
-    request = request.decode()                ## convert query to ASCII
+    request = request.decode()                  ## convert query to ASCII
     print(f'received request:\n{request}\n')
 
     ## unpack query
     query_string = request.split('\n')           ## separate headers in the request
     qbytes = int(query_string[1].split(': ')[1]) ## get number of bytes (query length)
-    query1 = query_string[0][-qbytes:]            ## get the last qbytes characters as query
-    query = query1.replace("?", ".")
-    query = f'{query}'
-    # query = re.escape(query, special_only=False)                    ## so regex characters don't break query
+    original_query = query_string[0][-qbytes:]   ## get the last qbytes characters as query
+
+    ## reformat query for regex
+    query = original_query.replace("?", ".")     ## replace all ? with . for regex
     
     ## note: we don't need to do anything with Connection-type, since we only support
     ## single-request queries for this assignment
 
-    # print(query, qbytes)
-
     ## get words that match the regex query expression
     matches_tf = DF.str.contains(query, case=False) ## perform regex for series (true/false) object  
-    # x = re.findall(query, DF.str)
-    # print(x)
-
-    # print(query)
-    # matches = DF.str.extractall(r'query')
-    # num_matches = len(matches)
-
     num_matches = matches_tf.sum()                  ## count number of matches in series
-
     matches = DF[matches_tf.fillna(False)]          ## get series containing all matching words
     print(matches.values)
 
-    # print(len(matches_tf.notna()))
-    # print(len(matches_tf))
-    # print(len(DF))
     ## output and return result
     print(f'found {num_matches} matches for {query}.')  
-    # print(len(matches))
-    return num_matches, matches.values, query1
+    
+    return num_matches, matches.values, original_query
 
-def sendResponse(serverSocket, connection, num_matches, matches, query):
+def sendResponse(connection, num_matches, matches, query):
     ''' send query response to client '''
+
+    ## set response values for case 00
     rcode = 00  ## no matches found, default
     rmsg = 'No matches found'
+
+    ## set response values for case 11
     if num_matches > 0:
         rcode = 11  ## matches found
         rmsg = f'Success: Found {num_matches} matches for {query}.'
+
+    ## generate full response message
     resp_msg = f'Code {rcode}\n{rmsg}\n{matches}'
 
     ## send query through the server socket            
@@ -110,14 +110,19 @@ def sendResponse(serverSocket, connection, num_matches, matches, query):
 
 def dispatcher(serverSocket):
     ''' accept connections from clients '''
+    
+    ## print to console
     print("\nserver started successfully!")
+
+    ## call handler method for each incoming connection
     while True:                                     ## wait for next connection
         print("\nwaiting for connections...", end=' ')
         connection, address = serverSocket.accept() ## accept connection
-        print(f"server connected to {address}\n")     ## output connection info
-        handleClient(connection, serverSocket)                    ## pass off to handleClient
-        
+        print(f"server connected to {address}\n")   ## output connection info
+        handleClient(connection, serverSocket)      ## pass off to handleClient
 
+
+## driver code
 startServer()
 
 
